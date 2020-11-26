f274930aa1dff5c073b4c7ce9bdff4581e4a4d59
AbdelrhmanAmin <maxhd213@gmail.com>
2020-11-26 23:35:37 +0200

    Fix linters + Add new inject

--- index.rb
+++ index.rb
@@ -18,129 +18,95 @@ module Enumerable
     arr = to_a
     (0..(arr.length - 1)).each do |i|
       yield(arr[i], i)
+    end
   end
-  def my_select
-      return enum_for unless block_given?
 
-      newArr = []
-      my_each { |x| newArr << x if yield(x) }
-      newArr
-      end
+  def my_select
+    return enum_for unless block_given?
+
+    new_arr = []
+    my_each { |x| new_arr << x if yield(x) }
+    new_arr
   end
 
   def my_all?(param = nil)
     return my_all? { |obj| obj } unless block_given? || !param.nil?
 
     my_each do |elem|
-      if block_given? && !yield(elem)
-        return false
+      return false if block_given? && !yield(elem)
 
-      elsif param.is_a?(Class) && !elem.is_a?(param)
-        return false
-      elsif param.class == Regexp && !elem.to_s.match(param.to_s)
-        return false
-      end
+      return false if param.is_a?(Class) && !elem.is_a?(param)
+      return false if param.class == Regexp && !elem.to_s.match(param.to_s)
     end
-    true
+    end
+  true
   end
 
-  def my_any?(param = nil)
-    return my_any? { |obj| obj } unless block_given? || !param.nil?
+def my_any?(param = nil)
+  return my_any? { |obj| obj } unless block_given? || !param.nil?
 
-    my_each do |n|
-      if param.is_a?(Class) && n.is_a?(param)
-        return true
-
-      elsif !param.class == Regexp && n.to_s.match(param.to_s)
-        return true
-      elsif block_given? && yield(n)
-        return true
-      end
-    end
-    false
+  my_each do |n|
+    return true if param.is_a?(Class) && n.is_a?(param)
+    return true if !param.class == Regexp && n.to_s.match(param.to_s)
+    return true if block_given? && yield(n)
   end
-  def my_none?(param = nil)
-    return my_none? { |obj| obj } unless block_given? || !param.nil?
-    my_each do |n|
-      if param.is_a?(Class) && n.is_a?(param)
-        return false
-      elsif !param.class == Regexp && n.to_s.match(param.to_s)
-        return false
-      elsif block_given? && yield(n)
-        return false
-      end
-    end
-    true
-  end 
-  def my_count?(param= nil)
-    counter = 0
-    if !block_given? && param.nil?
-        return self.to_a.length
-    elsif block_given?
-        my_each { |num| counter+=1 if yield(num)}
-    else
-       my_each {|num| counter+=1 if param == num}
-    end
-    counter
-  end
-  def  my_map(base = nil)
-    return my_map{|obj| obj} unless block_given? || !base.nil?
-    newArr = []
-    if base 
-      my_each {|i| newArr.push(base[i])}
-    elsif block_given?
-      my_each {|i| newArr.push(yield(i))}
-    end
-    newArr
-  end
-  def my_inject(sum=nil, currentValue=nil)
-    # if the input is range
-    # create a var arr and loop through numbers 
-    # if sum is given then yield and assign the value to sum
-    # if sum is not given then  sum = 0 and yield the block
-    # 
-    if self.is_a?(Range)
-      arr = self.to_a
-      if sum != nil
-        arr.my_each do |num|
-          sum = yield(sum,num)
-        end
-      elsif sum == nil
-        sum = 0
-        arr.my_each do |num|
-          sum = yield(sum,num)
-        end
-      end
-      return sum
-    end
-    if self.is_a?(Array)
-      print self
-    end
-  end
+  false
 end
 
+def my_none?(param = nil)
+  return my_none? { |obj| obj } unless block_given? || !param.nil?
 
+  my_each do |n|
+    return false if param.is_a?(Class) && n.is_a?(param)
+    return false if !param.class == Regexp && n.to_s.match(param.to_s)
+    return false if block_given? && yield(n)
+  end
+  true
+end
 
-#  def my_inject(acc = nil #total
-#   , operator = nil) # currentValue
-#         if acc.is_a?(Symbol) #Typeof acc == symbol
-#             operator = acc 
-#             acc = nil
-#         end
-#         is_symbol = operator.is_a?(Symbol) # True / false
-#         raise LocalJumpError if !block_given? && !is_symbol #if no block is given bcuz of yield
-#         is_range = self.kind_of?(Range) # check of a range
-#         my_each_with_index do | n, i | # num, index
-#             if (is_range ? true : i == 0) && acc == nil
-#                 acc = n #give the acc the first num of the range array
-#             else
-#                 if operator != nil && is_symbol # if the current Elem is true and a symbol
-#                     operator = operator.to_proc #turn the elem to a proc
-#                     acc = operator.call(acc, n) # invoke the proc
-#                 else
-#                     acc = yield(acc, n) #if not a symbol then yield the block
-#                 end
-#             end
-#         end
-#         return acc
-#     end
+def my_count?(param = nil)
+  counter = 0
+  if !block_given? && param.nil?
+    return to_a.length
+  elsif block_given?
+    my_each { |num| counter += 1 if yield(num) }
+  else
+    my_each { |num| counter += 1 if param == num }
+  end
+
+  counter
+end
+
+def my_map(base = nil)
+  return my_map { |obj| obj } unless block_given? || !base.nil?
+
+  new_arr = []
+  if base
+    my_each { |i| new_arr.push(base[i]) }
+  elsif block_given?
+    my_each { |i| new_arr.push(yield(i)) }
+  end
+  new_arr
+end
+
+def my_inject(sum = nil, opt = nil)
+  flag = opt.is_a?(Symbol)
+  my_each_with_index do |elem, index|
+    sum = if index == 0 && sum.nil?
+            elem
+          else
+            if !opt.nil? && flag == true
+              opt.to_proc(sum, elem)
+            else
+              yield(sum, elem)
+                  end
+          end
+  end
+  sum
+end
+
+end
+
+def multiply_els(elemts)
+  elemts.my_inject { |acc, n| acc * n }
+end
--- test.rb
+++ test.rb
@@ -1,61 +1,64 @@
+# frozen_string_literal: true
 
 require_relative 'index.rb'
+
 puts '1.--------my_each--------'
 %w[Sharon Leo Leila Brian Arun].my_each { |friend| puts friend }
 
 puts '2.--------my_each_with_index--------'
-%w[Sharon Leo Leila Brian Arun].my_each_with_index { |friend, index| puts friend if index.even? }
+%w[Sharon Leo Leila Brian Arun].my_each_with_index do |friend, index|
+  puts friend if index.even?
+end
 
 puts '3.--------my_select--------'
 puts (%w[Sharon Leo Leila Brian Arun].my_select { |friend| friend != 'Brian' })
 
 puts '4.--------my_all--------'
-puts (%w[ant bear cat].my_all? { |word| word.length >= 3 }) #=> true
-puts (%w[ant bear cat].my_all? { |word| word.length >= 4 }) #=> false
-puts %w[ant bear cat].my_all?(/t/) #=> false
-puts [1, 2i, 3.14].my_all?(Numeric) #=> true
-puts [].my_all? #=> true
-
+puts (%w[ant bear cat].my_all? { |word| word.length >= 3 })
+puts (%w[ant bear cat].my_all? { |word| word.length >= 4 })
+puts %w[ant bear cat].my_all?(/t/)
+puts [1, 2i, 3.14].my_all?(Numeric)
+puts [].my_all?
 
 puts '5.--------my_any--------'
-puts (%w[ant bear cat].my_any? { |word| word.length >= 3 }) #=> true
-puts (%w[ant bear cat].my_any? { |word| word.length >= 4 }) #=> true
-puts %w[ant bear cat].my_any?(/d/) #=> false
-puts [nil, true, 99].my_any?(Integer) #=> true
-puts [nil, true, 99].my_any? #=> true
-puts [].my_any? #=> false
+puts (%w[ant bear cat].my_any? { |word| word.length >= 3 })
+puts (%w[ant bear cat].my_any? { |word| word.length >= 4 })
+puts %w[ant bear cat].my_any?(/d/)
+puts [nil, true, 99].my_any?(Integer)
+puts [nil, true, 99].my_any?
+puts [].my_any?
 
 puts '6.--------my_none--------'
- puts (%w[ant bear cat].my_none? { |word| word.length == 5 }) #=> true
- puts (%w[ant bear cat].my_none? { |word| word.length >= 4 }) #=> false
- puts %w[ant bear cat].my_none?(/d/) #=> true
- puts [1, 3.14, 42].my_none?(Float) #=> false
- puts [].my_none? #=> true
- puts [nil].my_none? #=> true
- puts [nil, false].my_none? #=> true
- puts [nil, false, true].my_none? #=> false
+puts (%w[ant bear cat].my_none? { |word| word.length == 5 })
+puts (%w[ant bear cat].my_none? { |word| word.length >= 4 })
+puts %w[ant bear cat].my_none?(/d/)
+puts [1, 3.14, 42].my_none?(Float)
+puts [].my_none?
+puts [nil].my_none?
+puts [nil, false].my_none?
+puts [nil, false, true].my_none?
 
 puts '7.--------my_count--------'
 arr = [1, 2, 4, 2]
-puts arr.my_count? #=> 4 arg==nil && !block_given
-puts arr.my_count?(2) #=> 2 arg == n
-puts (arr.my_count? { |x| (x % 2).zero? }) #=> 3 block_given?
+puts arr.my_count?
+puts arr.my_count?(2)
+puts (arr.my_count? { |x| (x % 2).zero? })
 
 puts '8.--------my_maps--------'
 my_order = ['medium Big Mac', 'medium fries', 'medium milkshake']
-puts (my_order.my_map { |item| item.gsub('medium', 'extra large') })
+puts (my_order.my_map { |item| item.gsub('medium', 'extra large')})
 puts ((0..5).my_map { |i| i * i })
 puts 'my_map_proc'
 my_proc = proc { |i| i * i }
-puts (1..5).my_map(my_proc) { |i| i + i }
+puts (1..5).my_map(&my_proc)
 
 puts '8.--------my_inject--------'
-puts (1..5).my_inject(1) { |product, n| product * n } #=> 120 #product=1 fractional
-puts ((1..5).my_inject { |sum, n| sum + n }) #=> 15
-longest = %w[ant bear cat].my_inject do |memo, word| # yield // proc invokation
+puts ((1..5).my_inject { |sum, n| sum + n })
+puts (1..5).my_inject(1) { |product, n| product * n }
+longest = %w[ant bear cat].my_inject do |memo, word|
   memo.length > word.length ? memo : word
 end
-puts longest #=> "bear"
+puts longest
 
-# puts 'multiply_els'
-# puts multiply_els([2, 4, 5]) #=> 40
+puts 'multiply_els'
+puts multiply_els([2, 4, 5])
