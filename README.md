1bc837b2dd57ffedc53999014be8bc04443e659c
AbdelrhmanAmin <maxhd213@gmail.com>
2020-11-26 23:45:56 +0200

    Fix linters 1.2

--- README.md
+++ README.md
@@ -11,3 +11,32 @@ In this project, I've built my own version of the built-in Ruby’s enumerable m
 - Count;
 - Map;
 - Inject.
+
+## Built With
+
+- Ruby
+
+## Getting Started
+
+- open CMD
+- git clone https://github.com/AbdelrhmanAmin/Microverse-Ruby-Bubble-sort.git
+- **Make sure Ruby is installed on your computer**
+- in terminal type: ruby bubble_sort.rb
+
+## Author
+
+😎 **Abdo Amin**
+
+- GitHub: [@Abdelrhman-Amin](https://github.com/AbdelrhmanAmin)
+- Twitter: [@Abdo Amin](https://twitter.com/AbdoAmi60489112)
+- LinkedIn: [@Abdo Amin](https://www.linkedin.com/in/abdo-amin-ab786a1b0/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/AbdelrhmanAmin/Microverse-Ruby-Bubble-sort/issues).
+
+## 📝 License
+
+This project is [MIT](./LICENSE) licensed.
--- test.rb
+++ test.rb
@@ -1,6 +1,4 @@
-# frozen_string_literal: true
-
-require_relative 'index.rb'
+require_relative 'index'
 
 puts '1.--------my_each--------'
 %w[Sharon Leo Leila Brian Arun].my_each { |friend| puts friend }
@@ -11,26 +9,26 @@ puts '2.--------my_each_with_index--------'
 end
 
 puts '3.--------my_select--------'
-puts (%w[Sharon Leo Leila Brian Arun].my_select { |friend| friend != 'Brian' })
+puts(%w[Sharon Leo Leila Brian Arun].my_select { |friend| friend != 'Brian' })
 
 puts '4.--------my_all--------'
-puts (%w[ant bear cat].my_all? { |word| word.length >= 3 })
-puts (%w[ant bear cat].my_all? { |word| word.length >= 4 })
+puts(%w[ant bear cat].my_all? { |word| word.length >= 3 })
+puts(%w[ant bear cat].my_all? { |word| word.length >= 4 })
 puts %w[ant bear cat].my_all?(/t/)
 puts [1, 2i, 3.14].my_all?(Numeric)
 puts [].my_all?
 
 puts '5.--------my_any--------'
-puts (%w[ant bear cat].my_any? { |word| word.length >= 3 })
-puts (%w[ant bear cat].my_any? { |word| word.length >= 4 })
+puts(%w[ant bear cat].my_any? { |word| word.length >= 3 })
+puts(%w[ant bear cat].my_any? { |word| word.length >= 4 })
 puts %w[ant bear cat].my_any?(/d/)
 puts [nil, true, 99].my_any?(Integer)
 puts [nil, true, 99].my_any?
 puts [].my_any?
 
 puts '6.--------my_none--------'
-puts (%w[ant bear cat].my_none? { |word| word.length == 5 })
-puts (%w[ant bear cat].my_none? { |word| word.length >= 4 })
+puts(%w[ant bear cat].my_none? { |word| word.length == 5 })
+puts(%w[ant bear cat].my_none? { |word| word.length >= 4 })
 puts %w[ant bear cat].my_none?(/d/)
 puts [1, 3.14, 42].my_none?(Float)
 puts [].my_none?
@@ -42,19 +40,19 @@ puts '7.--------my_count--------'
 arr = [1, 2, 4, 2]
 puts arr.my_count?
 puts arr.my_count?(2)
-puts (arr.my_count? { |x| (x % 2).zero? })
+puts(arr.my_count? { |x| (x % 2).zero? })
 
 puts '8.--------my_maps--------'
 my_order = ['medium Big Mac', 'medium fries', 'medium milkshake']
-puts (my_order.my_map { |item| item.gsub('medium', 'extra large')})
-puts ((0..5).my_map { |i| i * i })
+puts(my_order.my_map { |item| item.gsub('medium', 'extra large') })
+puts((0..5).my_map { |i| i * i })
 puts 'my_map_proc'
 my_proc = proc { |i| i * i }
 puts (1..5).my_map(&my_proc)
 
 puts '8.--------my_inject--------'
-puts ((1..5).my_inject { |sum, n| sum + n })
-puts (1..5).my_inject(1) { |product, n| product * n }
+puts((1..5).my_inject { |sum, n| sum + n })
+puts(1..5).my_inject(1) { |product, n| product * n }
 longest = %w[ant bear cat].my_inject do |memo, word|
   memo.length > word.length ? memo : word
 end
